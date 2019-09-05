48a8d63a2127a585981addd54204636f8a51988b
Colby <colbykrawczyk10@gmail.com>
2019-09-05 17:10:04 -0400

    Cleaned Up WebsiteCommand.java
    What I changed and why:
    
    - Changed "org.bukkit.command.CommandExecutor" to "CommandExecutor"
    - Removed unnecessary constructor
    - Removed SurpressWarnings
    - Added Override
    - Removed unnecessary return statement.
    - Removed unnecessary player object.
    - Cleaned up indentation.
    - Changed bracket style. If you perfer the old one, feel free to switch it back.

--- commands/WebsiteCommand.java
+++ commands/WebsiteCommand.java
@@ -1,29 +1,24 @@
 package me.refrac.linkscore.commands;
 
+import me.refrac.linkscore.LinksCore;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import me.refrac.linkscore.Main;
+public class WebsiteCommand implements CommandExecutor
+{
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+    {
+		if(!(sender instanceof Player))
+		{
+			Bukkit.getServer().getLogger().warning(LinksCore.plugin.getConfig().getString("NoConsole"));
+		}
 
-public class WebsiteCommand implements org.bukkit.command.CommandExecutor {
-	
-  public WebsiteCommand() {
-  }
-  
-@SuppressWarnings("unused")
-public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
-  {
-		if (!(sender instanceof Player)) {
-			Bukkit.getServer().getLogger().warning(Main.plugin.getConfig().getString("NoConsole"));
-			return true;
-		}
-	  Player p = (Player) sender;
-      sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Main.plugin.getConfig().getString("WebsiteMessage")));
-      return true;
-		}
-  {
-  }
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', LinksCore.plugin.getConfig().getString("WebsiteMessage")));
+        return true;
+    }
 }
