db10d52f1e100f38ed9211050fcac92af9fd86ec
Colby <colbykrawczyk10@gmail.com>
2019-09-05 17:08:58 -0400

    Cleaned Up StaffChatCommand.java
    What I changed and why:
    - Removed unneccessary Constructor
    - Moved all variables to lowerCamelCase
    - Removed uneccessary Code Blocks
    - Fixed return statements
    - Clearned up Indentation
    - Changed bracket style. If you perfer the other way, feel free to change it back.

--- commands/StaffChatCommand.java
+++ commands/StaffChatCommand.java
@@ -2,6 +2,7 @@ package me.refrac.linkscore.commands;
 
 import java.util.ArrayList;
 
+import me.refrac.linkscore.LinksCore;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -9,76 +10,58 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import me.refrac.linkscore.Main;
-
-public class StaffChatCommand implements CommandExecutor {
-
-	  public Main plugin;
+public class StaffChatCommand implements CommandExecutor
+{
+	public LinksCore plugin;
 	
-	  public StaffChatCommand() {
-	  }
+	public static ArrayList<Player> insc = new ArrayList<Player>();
 	
-	public static ArrayList<Player> Insc = new ArrayList<Player>();
-	
-	String Prefix = ChatColor.translateAlternateColorCodes('&', Main.plugin.getConfig().getString("Prefix"));
-	String Enabled = ChatColor.translateAlternateColorCodes('&', Main.plugin.getConfig().getString("Enabled"));
-	String Disabled = ChatColor.translateAlternateColorCodes('&', Main.plugin.getConfig().getString("Disabled"));
-	String NoPerm = ChatColor.translateAlternateColorCodes('&', Prefix + Main.plugin.getConfig().getString("NoPerm"));
+	String prefix = ChatColor.translateAlternateColorCodes('&', LinksCore.plugin.getConfig().getString("Prefix"));
+	String enabled = ChatColor.translateAlternateColorCodes('&', LinksCore.plugin.getConfig().getString("Enabled"));
+	String disabled = ChatColor.translateAlternateColorCodes('&', LinksCore.plugin.getConfig().getString("Disabled"));
+	String noPerm = ChatColor.translateAlternateColorCodes('&', prefix + LinksCore.plugin.getConfig().getString("NoPerm"));
 	
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		
-		if(cmd.getName().equalsIgnoreCase("staffchat")) {
-		
-			if (!(sender instanceof Player)) {
-				
-				Bukkit.getServer().getLogger().warning(Main.plugin.getConfig().getString("NoConsole"));
-				
-				return true;
-				
-			}
-		  Player p = (Player) sender;
-		  
-		if(args.length == 0) {
-			
-			if(!p.hasPermission("staffchat.use")) {
-				
-				p.sendMessage(NoPerm);
-				
-				return true;
-		}
-			
-			if(Insc.contains(p)) {
-		
-				Insc.remove(p);
-				p.sendMessage(Disabled);
-		
-		return true;
-			} else 
-				
-				Insc.add(p);
-				p.sendMessage(Enabled);
-				
-				return true;
-			}
-			if(args.length <= 1) {
-				
-				p.sendMessage(ChatColor.translateAlternateColorCodes('&', Main.plugin.getConfig().getString("Usage")));
-				
-				return true;
-			}
-			return false;
-		}
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
 	{
+		if(cmd.getName().equalsIgnoreCase("staffchat"))
 		{
+			if (!(sender instanceof Player))
+			{
+				Bukkit.getServer().getLogger().warning(LinksCore.plugin.getConfig().getString("NoConsole"));
+				return true;
+			}
+
+			Player p = (Player) sender;
+		  
+			if(args.length == 0) {
+
+				if (!p.hasPermission("staffchat.use")) {
+					p.sendMessage(noPerm);
+
+					return true;
+				}
+
+				if (insc.contains(p))
+				{
+					insc.remove(p);
+					p.sendMessage(disabled);
+
+					return true;
+				}
+				else
+				{
+					insc.add(p);
+					p.sendMessage(enabled);
+
+					return true;
+				}
+			}
+			else
+			{
+				p.sendMessage(ChatColor.translateAlternateColorCodes('&', LinksCore.plugin.getConfig().getString("Usage")));
+			}
+		}
+		return true;
 	}
-	{
-}
-	{
-}
-	{
-}
-}
-	return false;
-}
 }
